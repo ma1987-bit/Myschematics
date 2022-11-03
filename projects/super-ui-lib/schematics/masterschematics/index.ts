0df967998bdb95c4554d2a621989dec4a75b3ac0
mahmoud-realdolman <mahmoud.ahmed@inetum-realdolmen.world>
2022-11-03 14:50:11 +0100

    updated masterschematics

---
+++ projects/super-ui-lib/schematics/masterschematics/index.ts
@@ -0,0 +1,35 @@
+import {
+  Rule,
+  SchematicContext,
+  Tree,
+  schematic,
+  
+} from "@angular-devkit/schematics";
+import { RunSchematicTask } from "@angular-devkit/schematics/tasks";
+
+
+export function masterschematics(_options: any): Rule {
+  return (_tree: Tree, _context: SchematicContext) => {
+   const rule = schematic("childSchematic", _options)
+    _context.addTask(new RunSchematicTask("realdolmen", _options));
+
+    return rule;
+  };
+}
+
+
+
+
+
+/*import { Rule, SchematicContext, Tree,schematic,chain} from '@angular-devkit/schematics';
+
+//import { RunSchematicTask } from '@angular-devkit/schematics/tasks';
+
+export function parentschematics(_options: any): Rule {
+ 
+      return (_tree: Tree, _context: SchematicContext) => {
+        const rule =schematic("super-ui-component",_options);
+        const rule2 = schematic("realdolmen",_options)
+        return chain([rule,rule2]);
+  };
+}*/
